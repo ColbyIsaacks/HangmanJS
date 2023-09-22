bd17cceee4ed1c93c408dfbadb14f139e7b95d67
ColbyIsaacks <50007919+ColbyIsaacks@users.noreply.github.com>
2023-09-22 14:30:29 -0500

    Add files via upload
    Replaced text-based images with images created in ms paint

--- scripts/hangman.js
+++ scripts/hangman.js
@@ -1,38 +1,52 @@
 $(document).ready(function(){
 
     function addPart(strike){
-        if(strike == 0){
-          $("#headLine").html("          |       O");
-        }
-        else if(strike == 1){
-          $("#torsoLine").html("          |       ||");
-        }
-        else if(strike == 2){
-          $("#torsoLine").html("          |      /||");
-        }
-        else if(strike == 3){
-          $("#torsoLine").html("          |      /||\\");
-        }
-        if(strike == 4){
-          $("#legLine").html("          |\\      /");
-        }
-        if(strike == 5){
-          $("#legLine").html("          |\\      /\\");
-        }
-      }
+        console.log("Strike!");
+        const imgSrcArray = ["rope.png", "head.png", "body.png", "arm1.png",
+                           "arm2.png", "leg1.png", "leg2.png"];
+        $("#frameImg").attr("src", "/public/images/" + imgSrcArray[strike]);
+    }
 
-      function game(){
-        const wordBank = ["DOG", "PYTHON", "JAVASCRIPT", "ACCESSORY", "MASSIVE", "FOREST"];
+    function game(){
+        const placeBank = ["LONDON", "PARIS", "ROME", "CHICAGO", "NEW YORK", "LOS ANGELES", "AUSTIN",
+                          "DALLAS", "ROME", "BERLIN", "MOSCOW", "TOKYO", "BEIJING", "PYONGYONG",
+                          "GERMANY", "UNITED STATES", "FRANCE", "RUSSIAN", "JAPAN", "CHINA", "MEXICO",
+                          "PERU", "TEXAS"];
+        const animalBank = ["DOG", "KANGAROO", "CAT", "ELEPHANT", "FOX", "HORSE", "GIRAFFE", "RABBIT",
+                            "PENGUIN", "SKUNK", "ANTEATER"];
+        const spaceBank = ["PLUTO", "MILKY WAY", "SOLAR SYSTEM", "ANDROMEDA", "NEBULA", "JUPITER", "EARTH",
+                           "SATURN", "PULSAR", "COMET", "GAS GIANT", ];
+        const foodBank = ["PIZZA", "BREAD", "HAM", "MILKSHAKE", "BURGER", "CRACKER", "FORTUNE COOKIE", "CHEESE",
+                          "SPAGHETTI", "HOTDOG", "ICE CREAM", "SANDWICH"];
+        const transportBank = ["TRAIN", "BUS", "TOYOTA TOCOMA", "AIRPLANE", "CAR", "SUBWAY", "BOAT", "SHIP", "HELICOPTER",
+                               "TRUCK", "BICYCLE", "MOTORCYCLE"];
+        const jobBank = ["DOCTOR", "SALESMAN", "CHEF", "TEACHER", "NURSE", "ENGINEER", "PROFESSOR", "THERAPIST"];
+        const sportsBank = ["FOOTBALL", "SOCCER", "HOCKEY", "BASKETBALL", "FENCING", "TENNIS", "RUGBY", "SKATING",
+                            "LACROSSE", "MARTIAL ARTS", "BOWLING", "SWIMMING"];
 
+
+        const superBank = [placeBank, animalBank, spaceBank, foodBank, transportBank, jobBank, sportsBank];    
+        const hintBank = ["Geograpy", "Animal", "Space", "Food", "Transportation", "Occupation", "Sports"]                      
         var gameOver = false;
         var count = 0;
         var strike = 0;
+        var randomBank = Math.floor(Math.random() * superBank.length);
+        var wordBank = superBank[randomBank];
+
+        $("#hint").html("hint : " + hintBank[randomBank]);
+
         var word = wordBank[Math.floor(Math.random() * wordBank.length)];
+
         var blanks = "";
-        //console.log(word);
+        console.log(word);
 
         for(let i = 0; i < word.length; i++){
-            blanks += "-";
+            if(word[i] != " "){
+              blanks += "-";
+            }
+            else{
+              blanks += " ";
+            }
         }
 
         $("#reset").click(function(){
@@ -65,11 +79,14 @@ $(document).ready(function(){
 
                 if(blanks == word){
                     $("#winner").html("You win!!!");
+                    $("#mystery").css("color", "rgb(0,255,0)");
                     gameOver = true;
                 }
 
-                if(strike == 6){
+                if(strike == 7){
                     $("#winner").html("Better luck next time!");
+                    $("#mystery").html(word);
+                    $("#mystery").css("color", "rgb(220,0,0)");
                     gameOver = true;
                 }
             });
